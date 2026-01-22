958f7cbc4b515de4c66334cc10d6b19e31c2ec4e
Rodolfo Chaves <75285271+rhchaves@users.noreply.github.com>
2026-01-22 13:59:26 -0300

    API Clientes - Inicio da implementação

---
+++ src/services/DM.Cliente.API/Data/Repository/ClienteRepository.cs
@@ -0,0 +1,37 @@
+﻿using DM.Clientes.API.Models;
+using DM.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DM.Clientes.API.Data.Repository;
+
+public class ClienteRepository : IClienteRepository
+{
+    private readonly ClientesContext _context;
+
+    public ClienteRepository(ClientesContext context)
+    {
+        _context = context;
+    }
+
+    public IUnitOfWork UnitOfWork => _context;
+
+    public async Task<IEnumerable<Cliente>> ObterTodos()
+    {
+        return await _context.Clientes.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Cliente> ObterPorCpf(string cpf)
+    {
+        return await _context.Clientes.FirstOrDefaultAsync(c => c.Cpf.Numero == cpf);
+    }
+
+    public void Adicionar(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
---
+++ src/services/DM.Cliente.API/Models/IClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using DM.Core.Data;
+
+namespace DM.Clientes.API.Models;
+
+public interface IClienteRepository : IRepository<Cliente>
+{
+    void Adicionar(Cliente cliente);
+    Task<IEnumerable<Cliente>> ObterTodos();
+    Task<Cliente> ObterPorCpf(string cpf);
+}
